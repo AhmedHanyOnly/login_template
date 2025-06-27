edf8807c91ac9a8e88d56ae2bdb234cc8d38d87e
ahmed <mommentcraft@gmail.com>
2025-06-27 19:40:49 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,16 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', function () {
-    return redirect('/');
-});
 Route::get('/', function () {
     return view('front.login');
 });
+
 Route::get('/code', function () {
     return view('front.code');
 });
+
 Route::get('/submit', function () {
     return view('front.submit');
 });
+
 require __DIR__ . '/auth.php';
